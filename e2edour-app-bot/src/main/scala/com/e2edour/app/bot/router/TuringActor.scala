4cc4a800316cf6987ec3667dd3880cfa3a5a093f
KingEngine <wangkechaodream@163.com>
2016-06-13 14:58:37 +0800

    支持网易新闻的返回

--- e2edour-app-bot/src/main/scala/com/e2edour/app/bot/router/TuringActor.scala
+++ e2edour-app-bot/src/main/scala/com/e2edour/app/bot/router/TuringActor.scala
@@ -3,6 +3,7 @@ package com.e2edour.app.bot.router
 import com.alibaba.fastjson.JSONObject
 import com.e2edour.common.utils.SendUtil
 import com.turing.util._
+import org.slf4j.{Logger, LoggerFactory}
 
 /**
   * 图灵机器人
@@ -12,20 +13,32 @@ import com.turing.util._
   */
 object TuringActor {
 
+  private val logger: Logger = LoggerFactory.getLogger(getClass)
+
   def send(msg: String, userid: String): TuringRes = {
+
     val apiKey = "a7ab2971e79c5ed477482bdc6c581bda";
     val json = new JSONObject();
     json.put("key", apiKey);
     json.put("info", msg);
-    println(s"---------------[$userid] chat dialog---------------------")
-    println(s"[$userid] ask :$msg")
+    logger.info(s"---------------chat [$userid] dialog---------------------")
+    logger.info(s"[$userid] ask :[$msg]")
     json.put("userid", userid)
-    //请求图灵api
-    val result = SendUtil.sendPost("http://www.tuling123.com/openapi/api", json.toString());
-    println(s"turling answer :$result")
-    val obj: TuringRes = TuringJsonUtil.parse(result)
+    if ("网易新闻".equals(msg)) {
+      val res = new TuringRes;
+      res.setUrl("http://3g.163.com");
+      res.setCode(TuringTypeCode.type_200000)
+      res.setText("新闻")
+      return res
+    } else {
+      //请求图灵api
+      val result = SendUtil.sendPost("http://www.tuling123.com/openapi/api", json.toString());
+      logger.info(s"turling answer :[$result]")
+      val obj: TuringRes = TuringJsonUtil.parse(result)
+      return obj
+    }
     //判断code类型
-    obj
+
   }
 }
 
