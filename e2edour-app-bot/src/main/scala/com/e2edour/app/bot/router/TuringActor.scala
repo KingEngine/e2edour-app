ad18bc77e9306f8655e14e8d4044295d9dd1b252
KingEngine <wangkechaodream@163.com>
2016-06-14 16:30:39 +0800

    更加scala

--- e2edour-app-bot/src/main/scala/com/e2edour/app/bot/router/TuringActor.scala
+++ e2edour-app-bot/src/main/scala/com/e2edour/app/bot/router/TuringActor.scala
@@ -15,29 +15,31 @@ object TuringActor {
 
   private val logger: Logger = LoggerFactory.getLogger(getClass)
 
+  val apiKey = "a7ab2971e79c5ed477482bdc6c581bda";
+
   def send(msg: String, userid: String): TuringRes = {
 
-    val apiKey = "a7ab2971e79c5ed477482bdc6c581bda";
     val json = new JSONObject();
     json.put("key", apiKey);
     json.put("info", msg);
+    json.put("userid", userid)
+
     logger.info(s"---------------chat [$userid] dialog---------------------")
     logger.info(s"[$userid] ask :[$msg]")
-    json.put("userid", userid)
-    if ("网易新闻".equals(msg)) {
-      val res = new TuringRes;
-      res.setUrl("http://3g.163.com");
-      res.setCode(TuringTypeCode.type_200000)
-      res.setText("新闻")
-      return res
-    } else {
-      //请求图灵api
-      val result = SendUtil.sendPost("http://www.tuling123.com/openapi/api", json.toString());
-      logger.info(s"turling answer :[$result]")
-      val obj: TuringRes = TuringJsonUtil.parse(result)
-      return obj
+    //模式匹配
+    msg match {
+      case "网易新闻" =>
+        val res = new TuringRes;
+        res.setUrl("http://3g.163.com");
+        res.setCode(TuringTypeCode.type_200000)
+        res.setText("新闻")
+        res
+      case _ =>
+        val result = SendUtil.sendPost("http://www.tuling123.com/openapi/api", json.toString())
+        logger.info(s"turling answer :[$result]")
+        val obj: TuringRes = TuringJsonUtil.parse(result)
+        obj
     }
-    //判断code类型
 
   }
 }
